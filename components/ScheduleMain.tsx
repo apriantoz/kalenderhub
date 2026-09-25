85fec4ad6622ac5f6850e943e6a191dfc2bd33ab
Apriantoz <ekaaprianta@gmail.com>
2026-09-25 15:15:12 +0800

    Sebelum ngopi

--- components/ScheduleMain.tsx
+++ components/ScheduleMain.tsx
@@ -251,8 +251,8 @@ export default function ScheduleMain() {
           </Tabs>
         </Card>
 
-        <FooterHub />
       </div>
+      <FooterHub />
 
       <AlertDialog
         open={!!deleteTarget}
